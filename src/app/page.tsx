62bad6f08f4b47fe208d50da5f4b6acc07ddf07a
Marcus Rådell <marcus@radell.net>
2024-11-13 09:20:22 +0100

    add message for no ice creams found

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Ice creams</h1>;
+  return (
+    <>
+      <h1>Ice creams</h1>
+      <p>No ice creams found. Please add an ice cream.</p>
+    </>
+  );
 }
